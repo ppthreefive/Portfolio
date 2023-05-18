a9ce22e2fd715cb5708c4434c15721c007779ef7
Phillip Pham <ppthreefive@gmail.com>
2023-05-18 02:08:39 -0700

    Created social media icon bar component

---
+++ src/app/icon-bar/icon-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-bar',
+  templateUrl: './icon-bar.component.html',
+  styleUrls: ['./icon-bar.component.scss']
+})
+export class IconBarComponent {
+
+}
