05ff1f3cae99a9eb258677e528d2ebccb284118e
Phillip Pham <ppthreefive@gmail.com>
2023-05-18 02:07:52 -0700

    Created works component

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'works',
   templateUrl: './works.component.html',
-  styleUrls: ['./works.component.css']
+  styleUrls: ['./works.component.scss']
 })
 export class WorksComponent {
 
+  constructor(private window: Window) {
+  }
+
+  openGithubPage(keyword: string) {
+    switch (keyword) {
+      case 'wetha':
+        this.window.open('https://github.com/ppthreefive/Wetha-Android-App', '_blank');
+        break;
+      case 'arctic':
+        this.window.open('https://github.com/ppthreefive/Arctic-Sea-Ice-Small-Network-Analyzer-', '_blank');
+        break;
+    }
+  }
 }
