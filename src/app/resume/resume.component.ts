e16dfecd6470a50a02c0a2b62634cd346cc36e34
Phillip Pham <ppthreefive@gmail.com>
2023-05-18 02:07:17 -0700

    Created resume component

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import * as moment from "moment/moment";
+import * as humanizeDuration from "humanize-duration";
 
 @Component({
   selector: 'resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.css']
+  styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent {
+  step: number;
+  isMobile: boolean;
 
+  constructor() {
+    this.onResize();
+  }
+
+  @HostListener("window:resize", [])
+  onResize() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  countYears(dateOne: string, dateTwo: string | null): string {
+    let dateEnd: moment.Moment;
+    let dateStart: moment.Moment;
+
+    if (dateTwo === null) {
+      dateEnd = moment();
+      dateStart = moment(dateOne);
+      return humanizeDuration(moment.duration(dateEnd.diff(dateStart)).asMilliseconds(), {largest: 2, round: true});
+    }
+    else {
+      dateEnd = moment(dateTwo);
+      dateStart = moment(dateOne);
+      return humanizeDuration(moment.duration(dateEnd.diff(dateStart)).asMilliseconds(), {largest: 2, round: true});
+    }
+  }
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  downloadResume() {
+    let link = document.createElement("a");
+    link.download = "PhillipPham_Resume";
+    link.href = "assets/resume/PhillipPham_Resume.pdf";
+    link.click();
+  }
 }
